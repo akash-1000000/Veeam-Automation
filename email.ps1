10a5e6f5f069350d9d11e9f5ba089c3b43f942c6
Akash-1000000 <uniyalakash25@gmail.com>
2024-01-27 13:10:21 +0530

    Adding 3 scripts i.e. free-space which reterieves storage on the disk, email this sends me email notification of the result file i.e. generated by backup-status script, finally I run all 3 scripts in final.ps1

---
+++ email.ps1
@@ -0,0 +1,6 @@
+﻿#Storing password for support@v-connect.in $pass variable.
+$pass = Get-StoredCredential -Target 'v-connect.in'
+#Changinf the security protocol to Tls12
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#Send-MailMessage with attachment
+Send-MailMessage -UseSsl -Credential $pass -From 'support@v-connect.in' -To 'akash.uniyal@v-connect.in' -Subject '192.168.10.111' -Body 'Result' -SmtpServer 'smtp.office365.com' -Attachments 'C:\Users\Administrator\Desktop\Veeam-automation\result.txt' 
---
+++ final.ps1
@@ -0,0 +1,3 @@
+﻿.\free-space.ps1 | Out-File .\result.txt
+.\backup-server-status.ps1 | Out-File -Append .\result.txt
+.\email.ps1
